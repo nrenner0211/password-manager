b44ab1b448b0a73e221546c92f81f60d924cab7a
nrenner <nrenner0211@gmail.com>
2022-08-18 22:36:13 -0400

    seeds

---
+++ seeds/items.sql
@@ -0,0 +1,36 @@
+-- items
+{
+    "title": "Facebook Password",
+    "content": "password1234",
+    "user_id": 1
+},
+{
+    "title": "Myspace Password",
+    "content": "tomfrommyspace",
+    "user_id": 1
+},
+{
+	"title": "github password",
+	"content": "jackrabbit",
+	"user_id": 1
+},
+{
+    "title": "Facebook Password",
+    "content": "password0987",
+    "user_id": 2
+},
+{
+    "title": "Myspace Password",
+    "content": "kachow69",
+    "user_id": 2
+},
+{
+    "title": "random important number",
+    "content": "123-456-7890",
+    "user_id": 1
+},
+{
+    "title": "top secret code",
+    "content": "0000-1234",
+    "user_id": 1
+}
---
+++ seeds/users.sql
@@ -0,0 +1,11 @@
+-- users
+{
+	"username": "lernantino",
+	"email": "lernantino@gmail.com",
+	"password": "lernantino"
+},
+{
+    "username": "carolj99",
+	"email": "caroljenkins@gmail.com",
+	"password": "iheartcats"
+}
